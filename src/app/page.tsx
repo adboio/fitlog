ed38a789a473e9e71898d6b9c4ff5ae3dc822ff0
Adam Bowker <adambowker98@gmail.com>
2025-05-04 21:56:24 -0700

    remove new tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -172,7 +172,6 @@ export default function Home() {
       <div className="w-full flex justify-start">
         <a
           href="https://adboio.fit"
-          target="_blank"
           rel="noopener noreferrer"
           className="mb-4 px-4 py-2 rounded-md bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors"
         >
