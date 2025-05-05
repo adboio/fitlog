02e6edff3fa588f8b93f97ad54ea54648a5510cf
Adam Bowker <adambowker98@gmail.com>
2025-05-04 22:09:52 -0700

    fix css

--- src/app/workout/[date]/page.tsx
+++ src/app/workout/[date]/page.tsx
@@ -38,7 +38,7 @@ export default function WorkoutPageWrapper(props: { params: Params }) {
   }
 
   return (
-    <div className="p-8 max-w-xl mx-auto">
+    <div className="p-8 max-w-xl mx-auto workout">
       <button
         onClick={() => router.back()}
         className="mb-4 px-4 py-2 rounded-md bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors"
